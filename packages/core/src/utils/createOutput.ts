91d30d9a9b61aa1cb65ae75a425fbaa9a0650640
‘Maxim <maxval1991@gmail.com>
2022-12-15 10:34:39 +0200

    feat(core): added htmlOutputType in config

--- packages/core/src/utils/createOutput.ts
+++ packages/core/src/utils/createOutput.ts
@@ -1,31 +1,43 @@
-import { BuilderOutput, Output } from "../types/types";
+import { BuilderOutput, HtmlOutputType, Output } from "../types/types";
 
 const createDoc = (data: { html: string; styles?: string; scripts?: string }): string => {
   const { html, styles = "", scripts = "" } = data;
   return `<!DOCTYPE html><html><head>${styles}</head><body>${html}${scripts}</body></html>`;
 };
 
-export const createOutput = (output: BuilderOutput): Output => {
+export const createOutput = <T extends HtmlOutputType>(type: T, output: BuilderOutput): Output<T> => {
   const { pageData, projectData, styles, scripts, html, error } = output;
-  const data: Output = {
+  const data = {
     pageData,
     projectData,
   };
 
   if (Array.isArray(scripts) && Array.isArray(styles) && html) {
-    return {
-      ...data,
-      html: createDoc({
-        html,
-        styles: styles.join(""),
-        scripts: scripts.join(""),
-      }),
-      htmlBuilder: {
-        styles: styles,
-        root: html,
-        scripts: scripts,
-      },
-    };
+    switch (type) {
+      case "monolith": {
+        const output = {
+          ...data,
+          html: {
+            root: createDoc({ html, styles: styles.join(""), scripts: scripts.join("") }),
+          },
+        };
+
+        return output as Output<T>;
+      }
+      case "partial": {
+        return {
+          ...data,
+          html: {
+            styles: styles,
+            root: html,
+            scripts: scripts,
+          },
+        };
+      }
+      default: {
+        console.error("Unknown type, the type must be: monolith or partial");
+      }
+    }
   }
 
   return { ...data, error };
