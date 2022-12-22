b51fb585a12c8cf0a344a5cbac7b6fee9bb8e5bd
‘Maxim <maxval1991@gmail.com>
2022-12-22 17:07:23 +0200

    fix(core): added media handler config

--- packages/core/src/actions/types.ts
+++ packages/core/src/actions/types.ts
@@ -2,4 +2,7 @@ export enum ActionTypes {
   save = "@builder_save",
   initPage = "@builder_init_page",
   onLoad = "@builder_on_load",
+  addMedia = "@builder_add_media",
+  addMediaRes = "@builder_add_media_res",
+  addMediaRej = "@builder_add_media_rej",
 }
