f644e54fbb70fa8a00d089ee103aba481efab3c3
victor-pg <victor.poalelungi1@gmail.com>
2023-02-09 11:51:11 +0200

    get output in save method

--- packages/core/src/actions/save.ts
+++ packages/core/src/actions/save.ts
@@ -1,7 +1,7 @@
 import { ActionResolve, Target } from "../types/types";
 import { ActionTypes } from "./types";
 
-export const save = (): ActionResolve => ({
+export const save = (data: { callback: string }): ActionResolve => ({
   target: Target.builder,
-  data: JSON.stringify({ type: ActionTypes.save }),
+  data: JSON.stringify({ type: ActionTypes.save, data }),
 });
