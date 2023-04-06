39fba093c4f0871ace133efa786eee0245b113e6
Maxim Valuta <maxval1991@gmail.com>
2023-04-06 17:55:30 +0300

    new(core): modulesGroup in ui.leftSidebar

--- packages/core/src/types/types.ts
+++ packages/core/src/types/types.ts
@@ -145,6 +145,64 @@ export interface Theme {
 
 //#endregion
 
+//#region ElementTypes
+
+export enum BaseElementTypes {
+  Text = "Text",
+  Image = "Image",
+  Button = "Button",
+  Icon = "Icon",
+  Spacer = "Spacer",
+  Map = "Map",
+  Form2 = "Form2",
+  Line = "Line",
+  Menu = "Menu",
+  ImageGallery = "ImageGallery",
+  Video = "Video",
+  Audio = "Audio",
+  VideoPlaylist = "VideoPlaylist",
+  IconText = "IconText",
+  Lottie = "Lottie",
+  Embed = "Embed",
+  StarRating = "StarRating",
+  Alert = "Alert",
+  Counter = "Counter",
+  Countdown2 = "Countdown2",
+  ProgressBar = "ProgressBar",
+  Calendly = "Calendly",
+  Carousel = "Carousel",
+  Tabs = "Tabs",
+  Accordion = "Accordion",
+  Switcher = "Switcher",
+  Table = "Table",
+  Timeline = "Timeline",
+  Facebook = "Facebook",
+  Twitter = "Twitter",
+  FacebookComments = "FacebookComments",
+  Columns = "Columns",
+  Row = "Row",
+}
+
+export enum StoryElementTypes {
+  StoryButton = "StoryButton",
+  StoryIcon = "StoryIcon",
+  StoryEmbed = "StoryEmbed",
+  StoryText = "StoryText",
+  StoryMap = "StoryMap",
+  StoryProgressBar = "StoryProgressBar",
+  StoryLine = "StoryLine",
+  StoryCountdown2 = "StoryCountdown2",
+  StoryCounter = "StoryCounter",
+  StoryShape = "StoryShape",
+  StoryForm2 = "StoryForm2",
+  StoryStarRating = "StoryStarRating",
+  StoryLottie = "StoryLottie",
+  StoryImage = "StoryImage",
+  StoryVideo = "StoryVideo",
+}
+
+//#endregion
+
 export type Response<R> = (r: R) => void;
 
 export type OnSave = <T extends HtmlOutputType>(output: Output<T>) => void;
@@ -223,6 +281,11 @@ export interface Config<T extends HtmlOutputType> {
           linkTarget?: "_blank" | "_self" | "_parent" | "_top";
         }>;
       };
+
+      modulesGroups?: Array<{
+        label: string;
+        modulesNames: Array<BaseElementTypes | StoryElementTypes>;
+      }>;
     };
 
     //#endregion
