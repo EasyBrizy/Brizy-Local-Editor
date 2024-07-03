cea7a67b58f98216735a20f25bc7fe1fd2468f6f
Max <zeetreex@gmail.com>
2024-07-03 07:41:50 +0300

    embedcode thirdparty

---
+++ packages/thirdparty-widgets/src/utils/constants/index.ts
@@ -0,0 +1,7 @@
+export const helperHTML = `
+<p class="brz-p">You can use the following selectors to create targeted CSS.</p>
+<p class="brz-p">
+  <span class="brz-span brz-ed-tooltip__overlay-code">element</span> {...}
+  <br class="brz-br">
+  <span class="brz-span brz-ed-tooltip__overlay-code">element .child-element</span> {...}
+</p>`;
---
+++ packages/thirdparty-widgets/src/utils/stateMode/index.ts
@@ -0,0 +1,5 @@
+export type State = "normal" | "hover" | "active";
+
+export const NORMAL: Extract<State, "normal"> = "normal";
+export const HOVER: Extract<State, "hover"> = "hover";
+export const ACTIVE: Extract<State, "active"> = "active";
