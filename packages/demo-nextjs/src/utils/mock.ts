362b1b2ec4b2d68f24ad05dd5989528dbb657b46
Maxim Valuta <maxval1991@gmail.com>
2024-07-23 11:24:40 +0300

    feat(nextjs): demo by env var

--- packages/demo-nextjs/src/utils/mock.ts
+++ packages/demo-nextjs/src/utils/mock.ts
@@ -6,7 +6,7 @@ export const projectId = 1;
 
 //#region Pages
 
-export enum PageIds {
+export enum VercelPageIds {
   HomePage = "1",
   IntegrationsPage = "6",
   CustomersPage = "7",
@@ -19,6 +19,21 @@ export enum PageIds {
   AnalyticsPage = "14",
   EdgePage = "15",
   StoragePage = "16",
+}
+
+export enum NetlifyPageIds {
+  HomePage = "17",
+  IntegrationsPage = "18",
+  CustomersPage = "19",
+  EnterprisePage = "20",
+  PricingPage = "21",
+  ContactPage = "22",
+  PreviewsPage = "23",
+  InfrastructurePage = "24",
+  NextjsPage = "25",
+  AnalyticsPage = "26",
+  EdgePage = "27",
+  StoragePage = "28",
 }
 
 //#region Story
@@ -37,5 +52,24 @@ export const popupPageId = "3";
 
 export const headerId = "4";
 export const footerId = "5";
+
+//#endregion
+
+//#region Config
+
+export function getPageIds() {
+  const type = process.env.DEMO_TYPE;
+
+  switch (type) {
+    case "vercel": {
+      return VercelPageIds;
+    }
+    case "netlify": {
+      return NetlifyPageIds;
+    }
+  }
+
+  throw Error(`Unknown demo page ${type}`);
+}
 
 //#endregion
