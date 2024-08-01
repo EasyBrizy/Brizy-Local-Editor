7b72b6aa994d00307736c6df2894b16d5de10d5d
GunkaArtur <arturel101010@mail.ru>
2024-08-01 12:06:17 +0200

    Removed feed widgets

--- packages/demo-nextjs/src/components/Metronic/partials/widgets/index.ts
+++ packages/demo-nextjs/src/components/Metronic/partials/widgets/index.ts
@@ -1,17 +1,5 @@
-// charts
-export * from "./feeds/FeedsWidget2";
-export * from "./feeds/FeedsWidget3";
-export * from "./feeds/FeedsWidget4";
-export * from "./feeds/FeedsWidget5";
-export * from "./feeds/FeedsWidget6";
-
 // tables
 export * from "./tables/TablesWidget10";
-
-// feeds
-export * from "./feeds/FeedsWidget2";
-export * from "./feeds/FeedsWidget3";
-export * from "./feeds/FeedsWidget4";
 
 // new
 export * from "./_new/cards/CardsWidget7";
