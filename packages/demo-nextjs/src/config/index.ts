122f8b396a81276d413f5addbc5f4d88b7812415
Maxim Valuta <maxval1991@gmail.com>
2024-08-16 16:56:56 +0300

    feat(nextjs): moved dnName to env

--- packages/demo-nextjs/src/config/index.ts
+++ packages/demo-nextjs/src/config/index.ts
@@ -1,6 +1,7 @@
 interface Config {
   host: string;
   dbUrl: string;
+  dbName: string;
   editorUrl: string;
   editorToken: string;
 }
@@ -11,6 +12,7 @@ export const getConfig = (): Config => {
   return {
     host: process.env["NEXT_PUBLIC_URL"] ?? DEFAULT_HOST,
     dbUrl: process.env["MONGODB_URI"] ?? "",
+    dbName: process.env["MONGODB_DATABASE_NAME"] ?? "",
     editorUrl: process.env["NEXT_PUBLIC_EDITOR_URL"] ?? "http://localhost:8001/index.js",
     editorToken: process.env["NEXT_PUBLIC_EDITOR_TOKEN"] ?? "demo",
   };
